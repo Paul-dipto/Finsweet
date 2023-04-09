89464eae4e76e9737721ec2979598c8d488f0b7a
Homelander-69 <abhinashpaul370@gmail.com>
2023-04-09 18:13:46 +0600

    read

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    slidesToshow: 2,
+    speed:1000,
+    arrows:true,
+    nextArrow: '<i class="fa-solid fa-chevron-right next"></i>',
+    prevArrow: '<i class="fa-solid fa-chevron-left prev"></i>',
+    autoplay: true,
+    autoplaySpeed: 3000,
+})
